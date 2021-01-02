173d3e50841c52121735a28465d255bba04ff8de
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2021-01-02 22:38:11 +0530

    testcase: 0,0,0,0 is to be worked on

--- arrays/3sum.cpp
+++ arrays/3sum.cpp
@@ -3,32 +3,43 @@
 */
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+// -1, -1, 0, 1, 2, 4
 std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+  if (nums.size() < 3) {
+    return {};
+  }
+  std::sort(nums.begin(), nums.end());
   std::vector<std::vector<int>> res = {};
   for (int i = 0;i < nums.size() - 2;i++) {
     std::vector<int> aTriplet = {};
     int target = nums[i];
-    std::cout << target << std::endl;
     int low = i + 1;
     int high = nums.size() - 1;
     int sum = 0 - nums[i];
     while (low < high) {
-      if (nums[low] + nums[high] == target) {
+      if (nums[low] + nums[high] == sum) {
         aTriplet.push_back(nums[i]);
         aTriplet.push_back(nums[low]);
         aTriplet.push_back(nums[high]);
+        break;
       }
-      // low++;
-      high--;
+      else if (nums[low] + nums[high] > sum) {
+        high--;
+      }
+      else {
+        low++;
+      }
     }
-    res.push_back(aTriplet);
+    if (aTriplet.size() > 0) {
+      res.push_back(aTriplet);
+    }
   }
   return res;
 }
 
 int main() {
-  std::vector<int> nums = { -1, 0, 1, 2, -1, -4 };
+  std::vector<int> nums = { 0,0,0,0 };
   std::vector<std::vector<int>> f = threeSum(nums);
 
   for (std::vector<int>& aTrip : f) {
