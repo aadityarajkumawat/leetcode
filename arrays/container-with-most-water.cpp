b10d906761090db1d31ac610cdba9e8820ba1fc3
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2021-01-02 17:28:17 +0530

    improved looping condition in traversing

--- arrays/container-with-most-water.cpp
+++ arrays/container-with-most-water.cpp
@@ -8,7 +8,7 @@
 int getMaxArea(std::vector<int>& height) {
   int i = height.size() - 1;
   int maxArea = 0;
-  for (int j = 0;j < height.size();) {
+  for (int j = 0;j < i;) {
     maxArea = std::max(maxArea, std::min(height[i], height[j]) * (i - j));
 
     if (height[i] > height[j]) {
@@ -23,7 +23,7 @@ int getMaxArea(std::vector<int>& height) {
 }
 
 int main() {
-  std::vector<int> height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+  std::vector<int> height = { 4, 3, 2, 1, 4 };
   int maxA = getMaxArea(height);
   std::cout << maxA << std::endl;
   return 0;
