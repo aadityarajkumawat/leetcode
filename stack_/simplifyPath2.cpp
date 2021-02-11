168e0cce23ed18b1f93797a51649c5daefd56aa0
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-11 20:34:08 +0530

    will come back after a few days

--- stack_/simplifyPath2.cpp
+++ stack_/simplifyPath2.cpp
@@ -17,13 +17,18 @@ std::string char_str(std::string path, int i) {
 std::string simplifyPath(std::string path) {
   std::stack <std::string> path_components;
   std::string curr = "";
+  if (char_str(path, path.length() - 1) != "/") {
+    path = path + "/";
+  }
   for (int i = 0;i < path.length();i++) {
     if (char_str(path, i) == "/") {
-      path_components.push(curr);
+      if (curr != "") {
+        path_components.push(curr);
+      }
       path_components.push(char_str(path, i));
       curr = "";
       if (i + 1 < path.length() && char_str(path, i + 1) == "/") {
-        i++;
+        path_components.pop();
       }
     }
     else if (char_str(path, i) != "/" && char_str(path, i) != ".") {
@@ -33,20 +38,46 @@ std::string simplifyPath(std::string path) {
       }
     }
     else if (char_str(path, i) == ".") {
-      if (i + 1 < path.length() && char_str(path, i + 1) == ".") {
-        path_components.pop();
-        path_components.pop();
-        if (path_components.size() > 0) {
+      //# ...
+      if (i + 2 < path.length() && char_str(path, i + 1) == "." && char_str(path, i + 2) == ".") {
+        path_components.push("...");
+        i = i + 2;
+      }
+      //# ../
+      if (i + 2 < path.length() && char_str(path, i + 1) == "." && char_str(path, i + 2) == "/") {
+        if (path_components.size() > 4) {
           path_components.pop();
+          path_components.pop();
+          path_components.pop();
+          std::cout << "exe" << std::endl;
         }
+        // else if (path_components.size() > 2) {
+        //   path_components.pop();
+        //   path_components.pop();
+        // }
         i++;
       }
+      //# .hidden
+      else if (i + 1 < path.length() && (char_str(path, i + 1) != "/" || char_str(path, i + 1) != ".")) {
+        path_components.push(".");
+      }
+      //# ..hidden
+      else if (i + 2 < path.length() && char_str(path, i + 1) == "." && (char_str(path, i + 2) != "/" || char_str(path, i + 2) != ".")) {
+        path_components.push("..");
+        i++;
+      }
+      //# ./
       else if (i + 1 < path.length() && char_str(path, i + 1) == "/") {
-        path_components.pop();
+        if (path_components.top() != "...") {
+          path_components.pop();
+        }
       }
     }
-
     showstack(path_components);
+  }
+
+  if (path_components.top() == "/" && path_components.size() > 1) {
+    path_components.pop();
   }
 
   std::string f = "";
@@ -54,11 +85,14 @@ std::string simplifyPath(std::string path) {
     f = path_components.top() + f;
     path_components.pop();
   }
+
   return f;
 }
 
+//##       "/", "c", "/"
+
 int main() {
-  std::string path = "/a/./b/../../c/";
+  std::string path = "/a/b/../../c/";
   std::string canonical_path = simplifyPath(path);
   std::cout << canonical_path << std::endl;
 
