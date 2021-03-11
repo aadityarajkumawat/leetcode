cdd2dd4f0e0713858fb8aa69650f3b4a6a6d8e4f
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-11 23:30:58 +0530

    finally completed simplyPath problem

--- stack_/simplify-path.cpp
+++ stack_/simplify-path.cpp
@@ -4,31 +4,52 @@
 
 std::string simplifyPath(std::string path)
 {
-    std::stack<char> stack;
+    std::stack<std::string> stack;
     std::string canonicalPath = "";
-    stack.push(path[0]);
-    for (int i = 1; i < path.length(); i++)
+
+    for (int i = 0; i < path.length(); i++)
     {
-        
+        if (path[i] == '/')
+            continue;
+
+        std::string dirName = "";
+
+        while (i < path.length() && path[i] != '/')
+        {
+            dirName += path[i];
+            i++;
+        }
+
+        i--;
+
+        if (dirName == ".")
+            continue;
+        if (dirName == "..")
+        {
+            if (!stack.empty())
+                stack.pop();
+            continue;
+        }
+
+        stack.push(dirName);
     }
 
-    if (stack.size() > 1 && stack.top() == '/')
-    {
-        stack.pop();
-    }
-
+    // get string answer
     while (!stack.empty())
     {
-        canonicalPath = stack.top() + canonicalPath;
+        canonicalPath = "/" + stack.top() + canonicalPath;
         stack.pop();
     }
+
+    if (canonicalPath.length() == 0)
+        return "/";
 
     return canonicalPath;
 }
 
 int main()
 {
-    std::string path = "/home//all/";
+    std::string path = "/a/./b/../../c/";
     std::string canonicalPath = simplifyPath(path);
     std::cout << canonicalPath << std::endl;
     return 0;
--- stack_/valid-parentheses.cpp
+++ stack_/valid-parentheses.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
-bool isValid(string s) {
-    stack<char> st;
-    st.push('*');
-    for(int i = 0;i < s.length(); i++) {
-	if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
-	    st.push(s[i]);
+bool isValid(std::string s)
+{
+	std::stack<char> st;
+	st.push('*');
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+		{
+			st.push(s[i]);
+		}
+		else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+		{
+			if (s[i] == ')' && st.top() == '(' && st.size() > 1)
+				st.pop();
+			else if (s[i] == '}' && st.top() == '{' && st.size() > 1)
+				st.pop();
+			else if (s[i] == ']' && st.top() == '[' && st.size() > 1)
+				st.pop();
+			else
+				return false;
+		}
 	}
-	else if(s[i] == ')' || s[i] == '}' || s[i] == ']') {
-	    if(s[i] == ')' && st.top() == '(' && st.size() > 1)
-		st.pop();
-	    else if(s[i] == '}' && st.top() == '{' && st.size() > 1)
-		st.pop();
-	    else if(s[i] == ']' && st.top() == '[' && st.size() > 1)
-		st.pop();
-	    else
-		return false;
-	}
-    }
 
-    return st.size() == 1;
+	return st.size() == 1;
 }
 
-int main() {
-    return 0;
+int main()
+{
+	return 0;
 }
